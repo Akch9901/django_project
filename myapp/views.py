e32e0034ccd69724eca4c217ca31cb5b7d8250e1
Akch9901 <akshaytondapi2001@gmail.com>
2025-03-13 20:13:08 -0500

    initial deploy

---
+++ myapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from .forms import EmployeeForm
+from .models import Employee
+
+def employee_list(request):
+    employees = Employee.objects.all()
+    return render(request, 'employee_list.html', {'employees': employees})
+
+def add_employee(request):
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('employee_list')
+    else:
+        form = EmployeeForm()
+    return render(request, 'add_employee.html', {'form': form})
